1ad73ecbd6bad4be30559caa9665c1ab356ac13b
NTatarnikov <t4t4rn1k0v@gmail.com>
2021-11-12 02:43:41 +0300

    Initial Commit

---
+++ SwiftUI-RockPaperScissors/SwiftUI_RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RockPaperScissorsApp.swift
+//  SwiftUI-RockPaperScissors
+//
+//  Created by Никита on 12.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
